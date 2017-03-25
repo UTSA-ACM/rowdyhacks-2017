3680fff3a1633b0ffbb5b289b1177a3103805f82
Andrew Hutton <fromalaska49@gmail.com>
2017-03-24 19:07:38 -0500

    Updated Registration

--- index.php
+++ index.php
@@ -21,6 +21,19 @@
 					<div class="row" style="overflow-y:hidden;">
 						<div class="col-sm-12" style="overflow-x:visible;">
 							<div class="row section-container">
+								<div class="col-sm-12" style="text-align:center;">
+									<h1 style="font-family:yellowtail;font-size:60px;margin:20px 20px 50px 20px;color:#256dad;">
+										RowdyHacks 2017
+									</h1>
+									<img src="images/RowdyHacks.png" style="max-width:80%;" />
+									<br />
+									<br />
+									<br />
+									<br />
+									<a class="typeform-share link" href="https://acm-utsa.typeform.com/to/pRCOFi" data-mode="popup" style="color:#267DDD;text-decoration:underline;font-size:20px;" target="_blank" ><input type="button" value="Register" /></a> <script> (function() { var qs, js, q, s, d = document, gi = d.getElementById, ce = d.createElement, gt = d.getElementsByTagName, id = "typef_orm_share", b = "https://s3-eu-west-1.amazonaws.com/share.typeform.com/"; if (!gi.call(d, id)) { js = ce.call(d, "script"); js.id = id; js.src = b + "share.js"; q = gt.call(d, "script")[0]; q.parentNode.insertBefore(js, q) } })() </script>
+								</div>
+							</div>
+							<div class="row section-container">
 								<div class="col-sm-12">
 									It’s official! Come join us at RowdyHacks as we build apps, games, and many other neat projects in 24 hours. Whether you’re a seasoned hacker or new to the hackathon community, we’ve got your back through the whole hacking experience.
 									<br />
